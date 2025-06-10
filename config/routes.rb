921d0fd37a99d979f361dbe98b51e4941195d722
Lucas Sousa <lucas.oliveira@reserva.ink>
2025-06-10 10:17:09 -0300

    feat: Add sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resource :session
-  resources :passwords, param: :token
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
